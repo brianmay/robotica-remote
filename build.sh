bde3cd013b5b8416d9c8f4e9eaccc61f3bceb09d
Brian May <brian@linuxpenguins.xyz>
2018-03-28 07:38:03 +1100

    Don't delete build directory between builds

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 set -ex
-rm -rf build
-mkdir build
+mkdir build || true
+mkdir build/uasyncio || true
 cp -a src/arequests.py src/main.py src/boot.py src/config.py \
     ./micropython-async/asyn.py \
     ./micropython-async/aswitch.py \
     ./micropython-mqtt/mqtt_as/mqtt_as.py \
     ./micropython-lib/typing/typing.py \
     build
-mkdir build/uasyncio
 cp -a \
     micropython-lib/uasyncio/uasyncio/__init__.py \
     micropython-lib/uasyncio.core/uasyncio/core.py \
